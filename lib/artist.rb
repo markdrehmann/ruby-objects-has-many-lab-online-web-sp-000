fa611972b3ea609849e550471bb291b755150964
Mark Drehmann <markdrehmann@gmail.com>
2020-03-02 16:36:47 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -15,6 +15,13 @@ class Artist
     @@song_count += 1
   end
 
+  def add_song_by_name(name)
+    song = Song.new(name)
+    @songs << song
+    song.artist = self
+    @@song_count += 1
+  end
+
   def self.song_count
     @@song_count
   end
