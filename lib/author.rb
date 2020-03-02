17c957395161243c1acbc6a455fcd725bf9728e4
Mark Drehmann <markdrehmann@gmail.com>
2020-03-02 17:26:39 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,19 +1,17 @@
 class Author
 
-  attr_accessor :name
+  attr_accessor :name, :posts
+  @@post_count = 0
 
   def initialize(name)
     @name = name
     @posts = []
   end
 
-  def posts
-    @posts
+  def add_post(post)
+    @posts << post
+    post.author = self
+    @@post_count += 1
   end
-
-  def self.song_count
-
-  end
-
 
 end
